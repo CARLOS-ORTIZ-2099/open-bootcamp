393d606a41da6de853604864f019c532c563a040
CARLOS EDUARDO ORTIZ <119457562+CARLOS-ORTIZ-2099@users.noreply.github.com>
2023-05-13 10:42:39 -0500

    Add files via upload

---
+++ numeros en js/archivo.js
@@ -0,0 +1,32 @@
+/* Crea un archivo JS que contenga las siguientes líneas
+
+- Una variable que contenga tu altura en centímetros (entero)
+
+- Una variable que contenga tu altura en metros (número de coma flotante)
+
+- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+
+- Una variable que contenga tu altura en metros redondeada hacia arriba
+
+- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+
+- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1"
+ es igual al máximo valor que se puede obtener en Javascript 
+ 
+*/
+
+
+
+let heightCentimetros = 180
+let heightMetros = 1.80
+let  weight = 60.5
+
+let redondeoHeight = Math.ceil(heightMetros)
+let redondeoweigth = Math.floor(weight)
+const max = Number.MAX_VALUE;
+const plusOne = max + 1;
+
+console.log(redondeoweigth)
+
+
+

ffa510c119935e99fd58dd98de6946bdfde0179f
CARLOS EDUARDO ORTIZ <119457562+CARLOS-ORTIZ-2099@users.noreply.github.com>
2023-05-16 10:33:53 -0500

    Add files via upload

---
+++ ejercicios sets y objetos/conjuntos.js
@@ -0,0 +1,13 @@
+/* - Un nuevo Set con los nombres de tu familia
+
+- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+
+- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;) */
+
+
+const family = new Set(['carlos','larisa','zulema','paul','jey'])
+console.log(family)
+
+family.add('carlos')
+family.add('javascript')
+console.log(family)

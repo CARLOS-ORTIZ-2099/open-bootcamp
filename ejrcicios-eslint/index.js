ea7e5655703168d1762d9cb9b405f9e6f56c847a
CARLOS EDUARDO ORTIZ <119457562+CARLOS-ORTIZ-2099@users.noreply.github.com>
2023-06-01 00:38:56 -0500

    Add files via upload

---
+++ ejrcicios-eslint/index.js
@@ -0,0 +1,42 @@
+const first = "l"
+
+// descativando reglas de eslint por lineas: esto se hace muy simple con comentarios
+
+// 1 FORMA
+
+/* eslint-disable */
+
+const letter = 'maria'
+
+/* eslint-enable */
+
+
+// 2 FORMA
+
+const newValor = 'p' // eslint-disable-line
+
+// 3 FORMA le decimos que debe oviar
+const newValor2 = 'p'; // eslint-disable-line quotes ,semi
+
+// 4 FORMA 
+
+/* eslint-disable-next-line indent,quotes  */
+    const farewell = 'chao'
+console.log("ere")
+
+const greeting = "hola"
+
+
+const name = "carlos"
+const lastname = "ortiz"
+const fullname = `${name} ${lastname}` 
+
+
+
+
+
+
+
+
+
+
